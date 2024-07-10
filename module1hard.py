18fea2cecdf1077a6275d30892a1a98548deee2f
Hendreik <slvl2@list.ru>
2024-07-10 10:58:27 +0300

    ver.2

--- module1hard.py
+++ module1hard.py
@@ -1,7 +1,7 @@
 # Дополнительное практическое задание по модулю: "Базовые структуры данных."
 import main
 
-main.print_hi('module1hard.py')  # .print_hi("PC")
+main.print_hi('module1hard.py')
 
 # Задание "Средний балл":
 # На вход даны следующие данные:
@@ -11,13 +11,9 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = sorted(students)
 #
-# dict1 = zip(students,grades)
-dict1 = {}
+dict1 = dict({})
 for n in range(students.__len__()):
-	grd = 0
-	print(sum(grades[n]))
 	dict1.update({students[n]: sum(grades[n]) / len(grades[n])})
-# print(students)
-# print(grades)
 
-print(dict1)
+### out:
+print('results ', dict1)
